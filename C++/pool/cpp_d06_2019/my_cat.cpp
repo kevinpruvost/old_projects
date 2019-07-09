65392eb282f00576d321f8de57a3e4885d1a95b2
pruvost kévin <kevin.pruvost@epitech.eu>
2019-07-09 20:31:09 +0200

    my_cat

--- C++/pool/cpp_d06_2019/my_cat.cpp
+++ C++/pool/cpp_d06_2019/my_cat.cpp
@@ -14,31 +14,42 @@ int get_file_size(ifstream *file)
     return (size);
 }
 
-int main(void)
+int my_cat(string filename)
 {
     char *content;
     ifstream file;
-    string filename;
-
-    cout << "Entrez le nom du fichier à lire : ";
-    cin >> filename;
-    cout << endl;
+    int file_size = 0;
 
     file.exceptions(ifstream::failbit | ifstream::badbit);
-    try
-    {
-        file.open(filename);
+        try
+        {
+            file.open(filename);
     }
     catch (ifstream::failure &e)
     {
         cerr << "Bug, le fichier ne peut pas être lu.\n";
         return (84);
     }
-    int file_size = get_file_size(&file);
+    file_size = get_file_size(&file);
 
     content = new char[file_size + 1];
     file.read(content, file_size);
-    printf("%s\n", content);
+    printf("%s", content);
     file.close();
+    return (0);
+}
+
+int main(int ac, char **av)
+{
+    string filename;
+
+    if (ac < 2)
+        return (84);
+    else {
+        for (int i = 1; i < ac; i++) {
+            filename = av[i];
+            my_cat(filename);
+        }
+    }
     return (0);
 }
